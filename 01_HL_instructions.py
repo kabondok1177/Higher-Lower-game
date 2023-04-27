2349c0a6f361306b6c209c679a0253e3cad6c5c3
kabondok1177 <125608943+kabondok1177@users.noreply.github.com>
2023-04-28 09:58:22 +1200

    Add files via upload
    Ask miss what to do after finished with instructions.

---
+++ 01_HL_instructions.py
@@ -0,0 +1,43 @@
+# Functions go here...
+# check user answer yes / no to a question
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+        if response == "yes" or response == "y":
+            return "yes"
+
+        elif response == "no" or response == "n":
+            return "no"
+
+        else:
+            print("Please answer yes / no")
+
+
+def instructions():
+    print()
+    print("**** Welcome to the Higher Lower Game ****")
+    print()
+    print("For each game you will be asked to...")
+    print("- Enter a 'low' and 'higher' number. The computer will randomly \n"
+          "generate a \n"
+          "'secret' number between your two chosen numbers. It will use \n"
+          "these numbers for all \n"
+          "the rounds in a given game.")
+    print("The computer will calculate how many guesses you are allowed")
+    print("enter the number of rounds you want to play")
+    print("guess the secret number")
+    print()
+    print("*** Good Luck! :)  ***")
+    print()
+    return ""
+
+
+instructions()
+
+# Main Routine goes here...
+played_before = yes_no("Have you played the "
+                       "game before? ")
+if played_before == "no":
+    instructions()
+
+    print("Program continues")
